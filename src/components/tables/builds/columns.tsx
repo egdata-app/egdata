a3fde397faaf288ab5dcdc63be0239561c1d5eeb
LeleDerGrasshalmi <lelegh@lel3x.de>
2026-07-11 14:31:37 +0200

    make display version more dynamic

--- src/components/tables/builds/columns.tsx
+++ src/components/tables/builds/columns.tsx
@@ -85,9 +85,10 @@ export const columns: ColumnDef<Build>[] = [
     enableColumnFilter: true,
     cell: (info) => {
       const version = info.getValue() as string;
-      const displayVersion = version.split("+")[0]
-        || version.replace('++Fortnite+Release-', '')
-        || version;
+      const displayVersion =
+        version.split("+")[0] ||
+        version.replace(`++${info.row.original.appName}+Release-`, "") ||
+        version;
 
       return (
         <Tooltip>
